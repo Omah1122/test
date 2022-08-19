bb92b4a548c8fd57a89183c2f677cd68e4fd1622
omah1122 <nwobodochioma027@gmail.com>
2022-08-19 11:51:36 +0100

    first commit

---
+++ js/custom.js
@@ -0,0 +1,51 @@
+
+// JS STYLING
+// var shape = document.getElementById('container');
+// shape.style.border ='2px solid black';
+// shape.style.height ='70vh';
+// shape.style.width ='100vw';
+// shape.style.borderRadius ='100px';
+
+// JS OUTPUTS
+// document.write('hello world')
+// window.alert('hello world')
+// prompt('hello world')
+// console.log('hello world')
+// document.getElementById('demo').innerHTML ='hello world'
+
+// JS OPERATORS
+// arithemetic operators e.g addition (+), substraction (-), multiplication(*), division(/), modulus (%)
+
+// comparison operators e.g equal ==, stricty equal to ===, greater than >, less than <, greater than or equal to >=, 
+// less than or equal to <=, not equal to !==, not strictly equal to !===
+
+// logical operators or || and &&
+
+// assignment operator equal sign =
+
+// JS DATA TYPES [primitive and complex/composite]
+// primitve 
+// string eg 'chioma',
+// numbers/intergers eg 1,2,4
+// float eg 1.2, 3.5
+// boolean 0, 1 (true, false)
+// null 
+
+// composite 
+// arrays ['chioma', 'dan', 'neche']
+// object {fname: 'chioma', lname: 'nwobodo', age: 10}
+
+// function  dan(){
+//     document.body.style.background = 'blue'
+// }
+
+// CONDITIONAL STATEMENT
+// if else elseif
+
+var number =10;
+if (number >6) {
+    console.log('true')
+}
+else if (number===5) {
+    console.log('false')
+}
